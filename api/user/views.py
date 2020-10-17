7784276e81d5e1364d0d10f08179182718c1382b
JaewonKwon <springkjw@gmail.com>
2020-10-17 14:45:09 +0900

    IsOwnerOrReadOnly 추가

--- api/user/views.py
+++ api/user/views.py
@@ -1,7 +1,10 @@
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
+from utils.permission import IsOwnerOrReadOnly
 
 
 class UserAPIView(GenericAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+
     def get(self, request, *args, **kwargs):
         return Response()
