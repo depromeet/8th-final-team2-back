1a65002f5577829cf95475546722e536d4897416
JaewonKwon <springkjw@gmail.com>
2020-10-17 14:53:03 +0900

    User Serializer 작성

--- api/user/views.py
+++ api/user/views.py
@@ -1,10 +1,14 @@
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from utils.permission import IsOwnerOrReadOnly
+
+from utils.permission import IsOwner
+from . import serializers
 
 
 class UserAPIView(GenericAPIView):
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [IsOwner]
+    serializer_class = serializers.UserSerializer
 
     def get(self, request, *args, **kwargs):
-        return Response()
+        serializer = self.get_serializer(instance=request.user)
+        return Response(serializer.data)
--- utils/permission.py
+++ utils/permission.py
@@ -1,9 +1,10 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.permissions import BasePermission
 
 
-class IsOwnerOrReadOnly(BasePermission):
+class IsOwner(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
         return obj.user == request.user
